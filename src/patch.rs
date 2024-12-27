357d10951e8ae93e32fa8c44f848286216c5607c
David Michael Barr <b@rr-dav.id.au>
2024-12-28 00:16:43 +0900

    Replace Vec for u32 buffers

--- src/patch.rs
+++ src/patch.rs
@@ -53,12 +53,12 @@ pub fn patch(old: &[u8], mut patch: &[u8], new: &mut Vec<u8>) -> io::Result<()> 
         .div_ceil(4);
     let delta_tags_len = deltas_len.div_ceil(4);
 
-    let control_tags = patch
+    let mut control_tags = patch
         .get(..control_tags_len)
         .ok_or(io::Error::from(UnexpectedEof))?;
     patch = &patch[control_tags_len..];
 
-    let delta_tags = patch
+    let mut delta_tags = patch
         .get(..delta_tags_len)
         .ok_or(io::Error::from(UnexpectedEof))?;
     patch = &patch[delta_tags_len..];
@@ -66,7 +66,7 @@ pub fn patch(old: &[u8], mut patch: &[u8], new: &mut Vec<u8>) -> io::Result<()> 
     if patch.len() < control_data_len {
         return Err(io::Error::from(UnexpectedEof));
     }
-    let control_data = patch;
+    let mut control_data = patch;
     patch = &patch[control_data_len..];
 
     let mut literals = patch
@@ -78,31 +78,37 @@ pub fn patch(old: &[u8], mut patch: &[u8], new: &mut Vec<u8>) -> io::Result<()> 
     if patch.len() < delta_data_len {
         return Err(io::Error::from(UnexpectedEof));
     }
-    let delta_data = patch;
+    let mut delta_data = patch;
     patch = &patch[delta_data_len..];
 
     let mut delta_diffs = patch
         .get(..deltas_len)
         .ok_or(io::Error::from(UnexpectedEof))?;
 
-    let buf_len = control_tags_len
-        .checked_add(delta_tags_len)
-        .ok_or(io::Error::from(InvalidData))?
-        .checked_mul(4)
-        .ok_or(io::Error::from(InvalidData))?;
-    let mut u32_buf = vec![0; buf_len];
-    let (controls, delta_skips) = u32_buf.split_at_mut(4 * control_tags_len);
-
-    let _ = coder.decode(control_tags, control_data, controls);
-    let controls = &controls[..controls_len * 3];
-    let _ = coder.decode(delta_tags, delta_data, delta_skips);
-    let mut delta_skips = &delta_skips[..deltas_len];
-
     let mut old_cursor: usize = 0;
     let mut delta_cursor: usize = 0;
     let mut stream_cursor: usize = 0;
 
-    for buffer in controls.chunks_exact(3) {
+    let mut controls_len = controls_len * 3;
+    let mut u32_buf = [0u32; 128];
+    let (mut control_begin, mut control_end) = (0, 0);
+    let (mut delta_begin, mut delta_end) = (96, 96);
+    while !control_tags.is_empty() || control_begin != control_end {
+        if control_begin == control_end {
+            let tags = control_tags.get(..24).unwrap_or(control_tags);
+            unsafe { std::hint::assert_unchecked(tags.len() <= 24) };
+            unsafe { std::hint::assert_unchecked(tags.len() <= control_tags.len()) };
+            (control_begin, control_end) = (0, tags.len() * 4);
+            let read = coder.decode(tags, control_data, &mut u32_buf[..control_end]);
+            unsafe { std::hint::assert_unchecked(read <= control_data.len()) };
+            control_end = controls_len.min(control_end);
+            controls_len -= control_end;
+            control_data = &control_data[read..];
+            control_tags = &control_tags[tags.len()..];
+        }
+        unsafe { std::hint::assert_unchecked(control_begin <= 96 - 3) };
+        let buffer = &u32_buf[control_begin..][..3];
+        control_begin += 3;
         let control: BsdiffControl = (&AehobakControl::try_from(buffer).unwrap()).into();
         let (add, copy) = (control.add as usize, control.copy as usize);
         let new_stream_cursor = stream_cursor
@@ -114,23 +120,41 @@ pub fn patch(old: &[u8], mut patch: &[u8], new: &mut Vec<u8>) -> io::Result<()> 
             .get(..add)
             .ok_or(io::Error::from(UnexpectedEof))?;
         let new_cursor = new.len();
-        new.extend(old_slice);
-        while !delta_skips.is_empty() && !delta_diffs.is_empty() {
-            let Some(new_delta_cursor) = delta_cursor.checked_add(delta_skips[0] as usize) else {
+        new.extend_from_slice(old_slice);
+        while !delta_diffs.is_empty() && (!delta_tags.is_empty() || delta_begin != delta_end) {
+            if delta_begin == delta_end {
+                let tags = delta_tags.get(..8).unwrap_or(delta_tags);
+                unsafe { std::hint::assert_unchecked(tags.len() <= 8) };
+                unsafe { std::hint::assert_unchecked(tags.len() <= delta_tags.len()) };
+                delta_begin = 96;
+                delta_end = delta_begin + tags.len() * 4;
+                let read = coder.decode(tags, delta_data, &mut u32_buf[delta_begin..delta_end]);
+                unsafe { std::hint::assert_unchecked(read <= delta_data.len()) };
+                delta_data = &delta_data[read..];
+                delta_tags = &delta_tags[tags.len()..];
+            }
+            unsafe { std::hint::assert_unchecked(delta_begin < 128) };
+            let Some(new_delta_cursor) = delta_cursor.checked_add(u32_buf[delta_begin] as usize)
+            else {
                 break;
             };
             if new_delta_cursor >= new_stream_cursor {
                 break;
+            }
+            unsafe {
+                std::hint::assert_unchecked(
+                    new_delta_cursor - stream_cursor + new_cursor < new.len(),
+                );
             }
             let new_byte = &mut new[new_delta_cursor - stream_cursor + new_cursor];
             *new_byte = new_byte.wrapping_add(delta_diffs[0]);
             delta_cursor = new_delta_cursor
                 .checked_add(1)
                 .ok_or(io::Error::from(InvalidData))?;
-            delta_skips = &delta_skips[1..];
+            delta_begin += 1;
             delta_diffs = &delta_diffs[1..];
         }
-        new.extend(literals.get(..copy).ok_or(io::Error::from(UnexpectedEof))?);
+        new.extend_from_slice(literals.get(..copy).ok_or(io::Error::from(UnexpectedEof))?);
         literals = &literals[copy..];
         stream_cursor = new_stream_cursor;
         old_cursor = usize::try_from(
