9e35ef752d88ddc8bc79f247234b62f23b503448
David Michael Barr <b@rr-dav.id.au>
2025-10-10 06:21:24 +0900

    Extract EncoderState for reuse

--- src/encode.rs
+++ src/encode.rs
@@ -35,79 +35,135 @@ pub fn encode<T: Write>(patch: &[u8], writer: &mut T) -> io::Result<()> {
 }
 
 fn encode_internal(mut patch: &[u8], writer: &mut dyn Write) -> io::Result<()> {
-    let mut literals = Vec::<u8>::new();
-    let mut seeks = Vec::<u32>::new();
-    let mut adds = Vec::<u32>::new();
-    let mut copies = Vec::<u32>::new();
-    let mut delta_skips = Vec::<u32>::new();
-    let mut delta_diffs = Vec::<u8>::new();
+    let mut encoder = EncoderState::new();
 
-    let mut add_cursor = 0;
-    let mut delta_cursor = 0;
     while 24 <= patch.len() {
         let control: AehobakControl = BsdiffControl::try_from(&patch[..24])
             .unwrap()
             .try_into()
             .unwrap();
-        control.encode((&mut adds, &mut copies, &mut seeks));
-        patch = &patch[24..];
         let (add, copy) = (control.add as usize, control.copy as usize);
-        for (idx, &delta) in patch[..add].iter().enumerate() {
-            if delta != 0 {
-                let skip = add_cursor + idx - delta_cursor;
-                delta_skips.push(skip.try_into().unwrap());
-                delta_diffs.push(delta);
-                delta_cursor += skip + 1;
-            }
-        }
-        add_cursor += add;
+        encoder.control(control);
+        patch = &patch[24..];
+        encoder.add_diffed(&patch[..add]);
         patch = &patch[add..];
-        literals.extend(&patch[..copy]);
+        encoder.copy(&patch[..copy]);
         patch = &patch[copy..];
     }
+    encoder.finalize(writer)
+}
 
-    let coder = Coder0124::new();
+pub struct EncoderState {
+    literals: Vec<u8>,
+    seeks: Vec<u32>,
+    adds: Vec<u32>,
+    copies: Vec<u32>,
+    delta_skips: Vec<u32>,
+    delta_diffs: Vec<u8>,
+    add_cursor: usize,
+    delta_cursor: usize,
+}
 
-    let controls = adds.len();
-    let padding = controls.wrapping_neg() % 4;
-    seeks.resize(controls + padding, 0);
-    adds.resize(controls + padding, 0);
-    copies.resize(controls + padding, 0);
+impl EncoderState {
+    pub fn new() -> Self {
+        Self {
+            literals: Vec::new(),
+            seeks: Vec::new(),
+            adds: Vec::new(),
+            copies: Vec::new(),
+            delta_skips: Vec::new(),
+            delta_diffs: Vec::new(),
+            add_cursor: 0,
+            delta_cursor: 0,
+        }
+    }
 
-    let padding = delta_skips.len().wrapping_neg() % 4;
-    delta_skips.resize(delta_skips.len() + padding, 0);
+    pub fn control(&mut self, control: AehobakControl) {
+        control.encode((&mut self.adds, &mut self.copies, &mut self.seeks));
+    }
 
-    let mut u32_seq = adds;
-    u32_seq.extend(&copies);
-    u32_seq.extend(&delta_skips);
-    u32_seq.extend(&seeks);
+    #[allow(unused)]
+    pub fn add(&mut self, old: &[u8], new: &[u8]) {
+        let add = old.len();
+        assert_eq!(add, new.len());
+        for (i, delta) in new
+            .iter()
+            .zip(old)
+            .map(|(n, o)| n.wrapping_sub(*o))
+            .enumerate()
+        {
+            if delta != 0 {
+                let skip = self.add_cursor + i - self.delta_cursor;
+                self.delta_skips.push(skip.try_into().unwrap());
+                self.delta_diffs.push(delta);
+                self.delta_cursor += skip + 1;
+            }
+        }
+        self.add_cursor += add;
+    }
 
-    let (tag_len, data_len) = Coder0124::max_compressed_bytes(u32_seq.len());
-    let mut encoded = vec![0u8; tag_len + data_len];
-    let (tags, data) = encoded.split_at_mut(tag_len);
-    let data_len = coder.encode(&u32_seq, tags, data);
-    let data = &data[..data_len];
+    pub fn add_diffed(&mut self, deltas: &[u8]) {
+        for (i, &delta) in deltas.iter().enumerate() {
+            if delta != 0 {
+                let skip = self.add_cursor + i - self.delta_cursor;
+                self.delta_skips.push(skip.try_into().unwrap());
+                self.delta_diffs.push(delta);
+                self.delta_cursor += skip + 1;
+            }
+        }
+        self.add_cursor += deltas.len();
+    }
 
-    let mut prefix = [0u8; 17];
-    let prefix_len = 1 + {
-        let (tag, data) = prefix.as_mut_slice().split_at_mut(1);
-        coder.encode(
-            &[
-                literals.len() as u32,
-                controls as u32,
-                delta_diffs.len() as u32,
-                data_len as u32,
-            ],
-            tag,
-            data,
-        )
-    };
+    pub fn copy(&mut self, new: &[u8]) {
+        self.literals.extend(new);
+    }
 
-    writer.write_all(&prefix[..prefix_len])?;
-    writer.write_all(&literals)?;
-    writer.write_all(tags)?;
-    writer.write_all(&delta_diffs)?;
-    writer.write_all(data)?;
+    pub fn finalize(&mut self, writer: &mut dyn Write) -> io::Result<()> {
+        let coder = Coder0124::new();
 
-    Ok(())
+        let controls = self.adds.len();
+        let padding = controls.wrapping_neg() % 4;
+        self.seeks.resize(controls + padding, 0);
+        self.adds.resize(controls + padding, 0);
+        self.copies.resize(controls + padding, 0);
+
+        let padding = self.delta_skips.len().wrapping_neg() % 4;
+        self.delta_skips.resize(self.delta_skips.len() + padding, 0);
+
+        let mut u32_seq = Vec::with_capacity(
+            self.adds.len() + self.copies.len() + self.delta_skips.len() + self.seeks.len(),
+        );
+        u32_seq.extend(&self.adds);
+        u32_seq.extend(&self.copies);
+        u32_seq.extend(&self.delta_skips);
+        u32_seq.extend(&self.seeks);
+
+        let (tag_len, data_len) = Coder0124::max_compressed_bytes(u32_seq.len());
+        let mut encoded = vec![0u8; tag_len + data_len];
+        let (tags, data) = encoded.split_at_mut(tag_len);
+        let data_len = coder.encode(&u32_seq, tags, data);
+        let data = &data[..data_len];
+
+        let mut prefix = [0u8; 17];
+        let prefix_len = 1 + {
+            let (tag, data) = prefix.as_mut_slice().split_at_mut(1);
+            coder.encode(
+                &[
+                    self.literals.len() as u32,
+                    controls as u32,
+                    self.delta_diffs.len() as u32,
+                    data_len as u32,
+                ],
+                tag,
+                data,
+            )
+        };
+
+        writer.write_all(&prefix[..prefix_len])?;
+        writer.write_all(&self.literals)?;
+        writer.write_all(tags)?;
+        writer.write_all(&self.delta_diffs)?;
+        writer.write_all(data)?;
+        Ok(())
+    }
 }
